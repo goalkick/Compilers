6f54c40cef790cb26cb285d07d14da28ead61946
good-mark <good-mark@mail.ru>
2015-12-01 14:16:25 +0300

    Book added

--- Compiler/Compiler/Translate.h
+++ Compiler/Compiler/Translate.h
@@ -1,14 +1,28 @@
+#include "IRTree.h"
+
 namespace Translate {
+
+	
 
 // объект-обёрткa над промежуточным результатом, который будет возвращать 
 //	правильный тип из Tree в зависимости от ситуации:
-class ISubtreeWrapper {
+	class ISubtreeWrapper {
+	public:
+		virtual ~ISubtreeWrapper() { }
+		virtual const IRTree::IExp* ToExp() const = 0; // как Expr
+		virtual const IRTree::IStm* ToStm() const = 0; // как Stm
+		virtual const IRTree::IStm* ToConditional(
+							 const Temp::CLabel* t, const Temp::CLabel* f) const = 0; // как if/jump
+	};
+
+class CExpConverter : public ISubtreeWrapper {
 public:
-	virtual ~ISubtreeWrapper() { }
-	virtual const IRTree::IExp* ToExp() const = 0; // как Expr
-	virtual const IRTree::IStm* ToStm() const = 0; // как Stm
-	virtual const IRTree::IStm* ToConditional(
-                         const Temp::CLabel* t, const Temp::CLabel* f) const = 0; // как if/jump
+	CExpConverter(const IRTree::Exp* e) : expr( e ) {}
+	const IRTree::IExp* ToExp() const {return expr;}
+	const IRTree::IStm* ToStm() const { ... ?... }
+	const IRTree::IStm* ToConditional(const CLabel* t, const CLabel* f) const { ... ?... }
+private:
+	const IRTree::IExp* expr;
 };
 
 }
