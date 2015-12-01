3e39f3f2780788269654597815302da9ff4fc6e9
Anton Zhidkov <goalkick94@gmail.com>
2015-12-01 17:20:41 +0400

    Add CTemp.cpp

---
+++ Compiler/Compiler/CTemp.cpp
@@ -0,0 +1,25 @@
+﻿#include "CTemp.h"
+
+namespace Temp {
+	int CLabel::nextUniqueId = 0;
+
+	CLabel::CLabel()
+	{
+		name = std::to_string( nextUniqueId++ );
+	}
+
+	CLabel::CLabel( const CSymbol& label ) :
+		name( label.GetString() )
+	{}
+
+	int CTemp::nextUniqueId = 0;
+
+	CTemp::CTemp()
+	{
+		name = std::to_string( nextUniqueId++ );
+	}
+
+	CTemp::CTemp( const CSymbol& symbol ) :
+		name( symbol.GetString() )
+	{}
+} // namespace Temp
