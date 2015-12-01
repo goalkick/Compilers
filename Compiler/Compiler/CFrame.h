c0f5376c8b2ff621d3ebe662643861cfb6040b1b
Anton Zhidkov <goalkick94@gmail.com>
2015-12-01 17:20:41 +0400

    Add CTemp.cpp

---
+++ Compiler/Compiler/CFrame.h
@@ -0,0 +1,22 @@
+﻿#include "CSymbol.h"
+#include <map>
+
+namespace Frame {
+	// Переменная фрейма
+	class IAccess {
+	public:
+		virtual ~IAccess() {}
+	};
+
+	// Класс-контейнер с платформо-зависимой информацией о функции
+	class CFrame {
+	public:
+		CFrame( const CSymbol* name, int formalsCount );
+		// Доступ к формальным параметрам
+		int FormalsCount() const { return formals.size(); }
+		const IAccess* Formal( CSymbol ) const;
+
+	private:
+		std::map<CSymbol, IAccess> formals;
+	};
+}
---
+++ Compiler/Compiler/CTemp.h
@@ -0,0 +1,36 @@
+﻿#include <string>
+#include "CSymbol.h"
+
+namespace Temp {
+	// Метка - точка перехода в коде
+	class CLabel {
+	public:
+		// Создать метку с уникальным именем
+		CLabel();
+		// Создать метку с заданным именем
+		explicit CLabel( const CSymbol& symbol );
+
+		const std::string& Name() const { return name; }
+
+	private:
+	// Счётчик для создания уникальных идентификаторов
+		static int nextUniqueId;
+		std::string name;
+	};
+	// Временная переменная
+	class CTemp {
+	public:
+		// Новая переменная с уникальным именем
+		CTemp();
+		// Новая переменная с заданным именем
+		explicit CTemp( const CSymbol& symbol );
+		~CTemp();
+
+		const std::string& Name() const { return name; }
+
+	private:
+		// Счётчик для создания уникальных имён
+		static int nextUniqueId;
+		std::string name;
+	};
+} // namespace Temp
